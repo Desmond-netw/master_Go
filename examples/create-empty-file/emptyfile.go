35ddd4b23053ca3869dc8eb5ef6282f0bfd6427a
Desmond-netw <ddeveh.study@gmail.com>
2025-10-05 15:30:28 +0000

    updated line comments

--- examples/create-empty-file/emptyfile.go
+++ examples/create-empty-file/emptyfile.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	// empty file creation
-	// using Creation () function
+	// function to create empty file
+	// create function can delete file and recreate it if it already eixst or the file will be created
 	myfile, err := os.Create("/home/analyst/myprojects/master_Go/examples/create-empty-file/data.txt")
 
 	if err != nil {
