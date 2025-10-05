26879e8787bb1fd6b509bbbbea5a4a334163eb77
Desmond-netw <ddeveh.study@gmail.com>
2025-10-05 15:31:06 +0000

    add a program to check file exist in a directory and print some information

---
+++ examples/check-file-exist/check-file-exist.go
@@ -0,0 +1,33 @@
+// Using IsNotExist to check wether file Exist or Not
+
+package main
+
+import (
+	"log"
+	"os"
+)
+
+// create a point to os.FileInfo
+var file *os.FileInfo
+var err error
+
+func main() {
+	// using Stat() to check file exist and file info
+	// the Stat() function can return file infor
+	// if the file does not exist it will return error msg.
+
+	//------------------
+	file, err := os.Stat("/home/analyst/myprojects/master_Go/examples/create-empty-file/dataa.txt") // specify file path
+
+	if err != nil {
+		// ------- checking if file exist
+		if os.IsNotExist(err) {
+			log.Fatal(err)
+		}
+	}
+
+	log.Println("File exists ")
+	log.Println("File details are : ")
+	log.Println("File name : ", file.Name())
+	log.Println("File size : ", file.Size())
+}
