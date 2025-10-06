014d8582f377b045c5627a4e18986eb0432976c0
Desmond-netw <ddeveh.study@gmail.com>
2025-10-06 18:54:51 +0000

    modify the log.Fatal function

--- examples/create-nested-dir/create-nested-dir.go
+++ examples/create-nested-dir/create-nested-dir.go
@@ -1,3 +1,17 @@
 // Creating Nested dir with os.MkdirAll ()
 
 package main
+
+import (
+	"log"
+	"os"
+)
+
+// using the os.mkdir and os.ModeParm
+func main() {
+	if es := os.MkdirAll("directory/sub1/sub2/sub3", os.ModePerm); es != nil {
+		if os.IsNotExist(es) {
+			log.Fatal(es)
+		}
+	}
+}
