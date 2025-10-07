838cfc0dcb531351aa83dfe57a234d6b2ff9d62a
Desmond-netw <ddeveh.study@gmail.com>
2025-10-07 17:44:45 +0000

    updated create and writeString fuction

---
+++ examples/create-writeString/create-writeString.go
@@ -0,0 +1,29 @@
+// Create file and writ content to it
+package main
+
+import (
+	"log"
+	"os"
+)
+
+// func main
+func main() {
+	// define string to be wrtien to file
+	var myString = "How I started learning kooding at kood/sisu"
+
+	// create a file using os.create function
+	file, err := os.Create("/home/analyst/myprojects/master_Go/examples/temp/test.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// write contents to the file
+	_, err = file.WriteString(myString)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatal(err)
+		}
+	}
+}
