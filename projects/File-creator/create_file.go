f8b7fd3c1449f74151d003fa4e2b04fe5f4ba629
Desmond-netw <ddeveh.study@gmail.com>
2025-10-08 18:44:57 +0000

    initial project with create file function

---
+++ projects/File-creator/create_file.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// Creat File function
+// it takes 2 arguments >> filename ja content
+func createFile(filename, content string) {
+	// printing operations
+	fmt.Println("Writing to file ......")
+
+	// using os.Create to truncate the file
+	path := filepath.Join("projects", "File-creator", "temp", filename)
+	myfile, es := os.Create(path)
+	if es != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file %v\n", es)
+		log.Fatal(es)
+		os.Exit(1)
+	}
+	defer myfile.Close()
+
+	// write content to the file
+	_, es = myfile.WriteString(content)
+	if es != nil {
+		fmt.Fprintf(os.Stderr, "Error writing to file %v\n", es)
+		log.Fatal(es)
+		os.Exit(1)
+	}
+	// success message
+	fmt.Printf("\t\v\nFile %s Writen successfully (%d bytes)", filename, myfile)
+}
