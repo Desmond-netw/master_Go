e6fe3f51e22caf6cb77155005cd54596a395b447
Desmond-netw <ddeveh.study@gmail.com>
2025-10-08 18:46:55 +0000

    Update project with file reading function

---
+++ projects/File-creator/read_file.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func readFile(filename string) {
+	fmt.Println("\nReading from file ......")
+
+	// Open function for reading
+	// for proper file path handling in all OS
+	path := filepath.Join("projects", "File-creator", "temp", filename)
+	myfile, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file : %v\n", err)
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer myfile.Close()
+
+	// read the file content
+	data, err := io.ReadAll(myfile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %V\n", err)
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	// success
+	fmt.Printf("Content of '%s': \n%s\n", filename, string(data))
+}
