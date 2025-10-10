088292dff1f9c9228c48bd955a594e6e607cd3c4
Desmond-netw <ddeveh.study@gmail.com>
2025-10-10 16:51:06 +0000

    initilized app to get using input from cli

---
+++ examples/001_read-line-text/main.go
@@ -0,0 +1,11 @@
+package main
+
+// cli tool to read line of text input from os.stdn
+func main() {
+	// step
+	/*
+		- get user input using bufio NewReader(os.Stdin)
+		- get store the input in variable call text
+		- print the text
+	*/
+}
