a523cb4f6c97588ee5e3078b1b39e4e2bf8ec86f
Desmond-netw <ddeveh.study@gmail.com>
2025-10-10 17:21:46 +0000

    completed application to take user input and display the input

--- examples/001_read-line-text/main.go
+++ examples/001_read-line-text/main.go
@@ -1,11 +1,26 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 // cli tool to read line of text input from os.stdn
 func main() {
 	// step
 	/*
 		- get user input using bufio NewReader(os.Stdin)
-		- get store the input in variable call text
+		-set variable for taking input
+		- store the input in variable call text
 		- print the text
 	*/
+
+	reader := bufio.NewReader(os.Stdin) // var reader
+
+	fmt.Println("Enter line of text")
+	text, _ := reader.ReadString('\n') // var to store user inputs
+
+	fmt.Printf("\nYou Ented: %s ", text)
+
 }
