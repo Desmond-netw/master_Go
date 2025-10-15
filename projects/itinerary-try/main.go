30f259de58f3aba7e02d1992a4acfff717181937
Desmond-netw <ddeveh.study@gmail.com>
2025-10-15 03:52:02 +0000

    initilize apport lookup func and add main to system folder

---
+++ projects/itinerary-try/main.go
@@ -0,0 +1,66 @@
+package main
+
+// imports
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// Application main entry
+func main() {
+	// set h flag helper to display application usage
+	help := flag.Bool("h", false, "Display Usage")
+	flag.Parse()
+
+	//if -h is passe, print usage and exit
+	if *help {
+		fmt.Println("itinerary usage:")
+		fmt.Println("go run . ./input.txt ./output.txt ./airport-lookup.csv")
+		return
+	}
+
+	// check if exactly 3 arguments are passed (input, output,, lookup)
+	args := flag.Args()
+	if len(args) != 3 {
+		fmt.Fprintln(os.Stderr, "Error : Expected 3 arguments")
+		fmt.Println("Usage: go run . ./input.txt ./output.txt ./airport-lookup.txt")
+		os.Exit(1)
+	}
+	// Assign variables to arguments
+	inputPath := args[0]
+	outPuth := args[1]
+	lookupPath := args[2]
+
+	//Load airport lookup data from CSV into a map
+	airportMap, err := loadAirportLookup(lookupPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error laoding airport lookup : %v\n", err)
+		os.Exit(1)
+	}
+
+	// Read the raw itinerary text from the input file 
+	inputData, err := os.ReadFile(inputPath)
+	if err != nil {
+		log.Fatal("Error Reading lookup file")
+		os.Exit(1)
+	}
+
+	// split the input into lines for processing
+	lines := strings.Split(string(inputData), "\n")
+	var outputLines []string
+
+	// process each line : replace airport coes with full names
+	for _, eachline := range lines {
+		for code, name := range airportMap {
+			if strings.Contains(eachline, code) {
+				eachline = strings.ReplaceAll(eachline, code, name)
+			}
+		}
+		outputLines = append(outputLines, eachline)
+	}
+	
+}
