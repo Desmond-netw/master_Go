b70875ffd8295916629990958d1277e308e4be75
Desmond-netw <ddeveh.study@gmail.com>
2025-10-15 18:28:53 +0000

    create lookupmap to store iata code and airpot names

--- projects/itinerary-try/loadAirportLookup.go
+++ projects/itinerary-try/loadAirportLookup.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // loading CSV File and return a map of csv code
@@ -24,4 +25,31 @@ func loadAirportLookup(filePath string) (map[string]string, error) {
 	if es != nil {
 		return nil, fmt.Errorf("error reading csv: %v", es)
 	}
+
+	//create a map to store airport codes names
+	lookupMap := make(map[string]string)
+	count := 0
+
+	// iterate through records and populate the map
+	for indx, eachRecode := range records {
+		//skip the header row
+		if indx == 0 {
+			continue // skip header
+		}
+		if len(eachRecode) < 5 {
+			continue // skip if not enough columns
+		}
+
+		// get iata code and airport name
+		iata := strings.TrimSpace(eachRecode[4])
+		name := strings.TrimSpace(eachRecode[0])
+
+		if iata != "" && name != "" {
+			lookupMap[iata] = name
+			count++
+		}
+	}
+
+	fmt.Printf("Succefully laoded %d airport codes from lookup file \n", count)
+	return lookupMap, nil
 }
