1032f9250190010731cf66ea9549f8cc5232f678
Desmond-netw <ddeveh.study@gmail.com>
2025-10-20 04:57:57 +0000

    simple var declaration app

---
+++ examples/005_understand-var/var_example1.go
@@ -0,0 +1,12 @@
+// The following program desmonstrates the use of variables in Go.
+package main
+
+import "fmt"
+
+func main() {
+	// Declare a variable 'message' of type string and assign a value to it
+	var message string = "Hello, Go Variables!"
+
+	// Print the value of the variable to the console
+	fmt.Println(message)
+}
