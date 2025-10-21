a34de2668fadb397411b548b248ac869fbabbe94
Desmond-netw <ddeveh.study@gmail.com>
2025-10-21 16:52:21 +0000

    golang Misc operators demo

---
+++ examples/misc_operators/miscOperator.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Program to demonstrate misc operators
+	// & and * operators
+
+	// declare variable
+	var a int = 58
+
+	// & operator returns the address of variable a
+	z := &a
+	fmt.Println("To get value of a print z:", z)
+
+	// * operator
+	fmt.Println("To get value of a print *z:", *z)
+
+}
---
+++ examples/misc_operators/readme.md
@@ -0,0 +1,4 @@
+## Example of Golang Misc Operators
+
+* `&:` This operator returns the variable’s address.
+* `*:` This operator returns a pointer to a variable.
