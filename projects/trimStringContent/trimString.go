1f23626fade984dfb0098cd51682144f1e79df0f
Desmond-netw <ddeveh.study@gmail.com>
2025-10-27 07:32:44 +0000

    Update: using trime functions

--- projects/trimStringContent/trimString.go
+++ projects/trimStringContent/trimString.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Program to illustrate string trim
 
 func main() {
@@ -8,5 +13,15 @@ func main() {
 
 	stry1 := "!! Welcome to Everyone !!"
 	stry2 := "@@This is example of Golang$$"
+
+	// Display strings
+	fmt.Println("Strings before the trimming:")
+	fmt.Println("String 1:", stry1)
+	fmt.Println("String 1:", stry2)
+
+	// Printing trim strings
+	// Using Trim() function
+	trim1 := strings.Trim(stry1, "!")
+	trim2 := strings.Trim(stry2, "@$")
 
 }
