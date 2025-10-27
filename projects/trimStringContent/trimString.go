160cdc90087a2e8f9e7fb893f86017195fac14b0
Desmond-netw <ddeveh.study@gmail.com>
2025-10-27 07:03:09 +0000

    initilize strings var

---
+++ projects/trimStringContent/trimString.go
@@ -0,0 +1,12 @@
+package main
+
+// Program to illustrate string trim
+
+func main() {
+	// creating, initializing strings
+	// Using shorthand methods
+
+	stry1 := "!! Welcome to Everyone !!"
+	stry2 := "@@This is example of Golang$$"
+
+}
