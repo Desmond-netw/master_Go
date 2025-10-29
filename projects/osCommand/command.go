16869981f02d6a4a4dded73b313e1e7b0c73a5f8
Desmond-netw <ddeveh.study@gmail.com>
2025-10-29 21:45:05 +0000

    update: reverse go command app

--- projects/osCommand/command.go
+++ projects/osCommand/command.go
@@ -1,23 +1,25 @@
+// package main
 package main
 
+// imports
 import (
 	"log"
 	"os"
 	"os/exec"
 )
 
+// a function to exe the command
+
 func main() {
+	// initilize command
+	command := "mkdir newDir"
 
-	// initilaize a command
-	command := "lsae"
-
-	// get the command exc object
+	// creating a command obj
 	cmd_obj := exec.Command(command)
-	cmd_obj.Stdout = os.Stdout
-
-	// edge cases
+	cmd_obj.Stdout = os.Stdout // standard output
+	// handle stderr
 	cmd_obj.Stderr = os.Stderr
-	err := cmd_obj.Run()
+	err := cmd_obj.Run() //edge cases
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
