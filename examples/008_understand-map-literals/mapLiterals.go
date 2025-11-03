ee0f1e2d05b5130ff468ea94b41fd085c39fe7aa
Desmond-netw <ddeveh.study@gmail.com>
2025-11-03 20:37:25 +0000

    checked if map is nil or not

--- examples/008_understand-map-literals/mapLiterals.go
+++ examples/008_understand-map-literals/mapLiterals.go
@@ -1,10 +1,17 @@
 package main
 
+import "fmt"
+
 // create, and initialize the maps
 //import package
 
 func main() {
 	// Creating  and initializing empty map.
 	var map_1 map[int]int
-
+	// check if the map is nil or not
+	if map_1 == nil {
+		fmt.Println("True")
+	} else {
+		fmt.Println("False")
+	}
 }
