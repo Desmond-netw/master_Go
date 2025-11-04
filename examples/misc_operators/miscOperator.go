8a60c5078764d5573861fc664894b362bef7df61
Desmond-netw <ddeveh.study@gmail.com>
2025-11-04 21:21:11 +0000

    modify printout for a address

--- examples/misc_operators/miscOperator.go
+++ examples/misc_operators/miscOperator.go
@@ -13,7 +13,7 @@ func main() {
 
 	// & operator returns the address of variable a
 	z := &a
-	fmt.Println("To get value of a print z:", z)
+	fmt.Println("To get the address of a print z :", z)
 
 	// * operator returns the pointer to the value of Z
 	fmt.Println("To get value of a print *z:", *z)
