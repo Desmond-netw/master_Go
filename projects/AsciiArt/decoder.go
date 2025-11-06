11f11a73c632895692b45a2ae21540e397b5fc40
Desmond-netw <ddeveh.study@gmail.com>
2025-11-06 05:59:43 +0000

    initilize pesudocode

---
+++ projects/AsciiArt/decoder.go
@@ -0,0 +1,12 @@
+package main
+
+// a func that will decode the ascil text
+/*
+	Steps:
+	-get input arguments from the command line [5 &] or [5 #][5 -_]-[5 #]
+	-format the input args by removing brackets '[' and ']'
+	-get content from input brackets
+	-check for the 1st number and multiply the next char with that number
+	-if multiple bracket, look for it and remove them.
+
+*/
