41e9b96810f7dd3e487fb6ed6241b66a0bb6889f
Desmond-netw <ddeveh.study@gmail.com>
2025-11-07 17:50:28 +0000

    added func to decode input from aguments

--- projects/AsciiArt/decoder.go
+++ projects/AsciiArt/decoder.go
@@ -1,12 +1,54 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
 // a func that will decode the ascil text
 /*
-	Steps:
-	-get input arguments from the command line [5 &] or [5 #][5 -_]-[5 #]
-	-format the input args by removing brackets '[' and ']'
-	-get content from input brackets
-	-check for the 1st number and multiply the next char with that number
-	-if multiple bracket, look for it and remove them.
+	Purpose: take args as input and decode it
+	Step: Fetch content from input
+	- set arguments
+	- remove unneeded brackets
+	-openBracket [
+	-closeBrackets ]
+	Step2: validate content
+	- spacePosition check
+	-conv anything before :spacePostion to int
+
 
 */
+func main() {
+	// parse flags
+	flag.Parse()
+
+	// Initilize flags for cli arguments
+	arg := flag.Args()
+	if len(arg) < 1 {
+		fmt.Printf("Usage: go run . '[5 #]' ")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// let set input to first arguments
+	input := arg[1]
+
+	// iterate through input to get content and check for openBracket and closeBrackets
+	// remove open brackets and closebrackest
+	for _, contents := range input {
+		content := string(contents)
+		cleanedContent := removeBrackets(content)
+		fmt.Println(cleanedContent)
+		return
+	}
+	return
+}
+
+// func to remove open and close brackets
+func removeBrackets(input string) string {
+	// Replaceall func to remove all brackets
+	return strings.ReplaceAll(strings.ReplaceAll(input, "[", ""), "]", "")
+}
