73d01bdfeb1e02e0fe5f23a3d8c666c41a4aa4bc
Desmond-netw <ddeveh.study@gmail.com>
2025-11-13 20:56:49 +0000

    example port scanner app

---
+++ examples/portScanner/portScanner.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+// To scan ip address open port
+// you need target ip address
+// and example port
+
+func main() {
+	IP := "scanme.nmap.org"
+	Port := "80" // that http port
+
+	// use net/Dial func
+	address := IP + ":" + Port
+	network := "tcp"
+	connection, err := net.Dial(network, address)
+	if err != nil {
+		fmt.Println("Error connection to:", IP)
+	}
+
+	fmt.Println("connecion ...", connection.RemoteAddr().s)
+
+}
