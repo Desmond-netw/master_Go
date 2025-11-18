d8b49914c2cd10d2ee470468234bd9f726fab0db
Desmond-netw <ddeveh.study@gmail.com>
2025-11-18 21:37:26 +0000

    initilize about handler

--- examples/GoWebServer/main.go
+++ examples/GoWebServer/main.go
@@ -10,10 +10,15 @@ func main() {
 	// Server main entry
 	staticPage := http.FileServer(http.Dir("./static"))
 	http.Handle("/", staticPage) //automatically get homepage
+	http.HandleFunc("/about", about)
 
 	fmt.Println("listening on port :8080")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+
 }
