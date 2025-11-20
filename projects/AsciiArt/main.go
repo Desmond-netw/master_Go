d3c6aec182e1a8b182f7428f76d545392085b409
Desmond-netw <ddeveh.study@gmail.com>
2025-11-20 17:15:16 +0000

    implemented flag validation and print results

--- projects/AsciiArt/main.go
+++ projects/AsciiArt/main.go
@@ -16,22 +16,61 @@ func main() {
 	// parse flags
 	flag.Parse()
 	// Initilize flags for cli arguments
-	arg := flag.Args()
-	if len(arg) < 1 {
-		fmt.Printf("Usage: go run . '[5 #]' ")
-		fmt.Println("Example: go run . '[5 #][5 -_]-[5 #]'")
-		flag.PrintDefaults()
-		os.Exit(1)
+	if *help {
+		displayHelp()
+		return
+	}
+	// check for multiline mode and handle flags
+	if *multiline {
+		if *encodeFlag {
+			encodeMultipleLines() // encode multiple lines of partten
+		} else {
+			// read and decode multiline mode from stdin
+			decodeMultipleLines()
+		}
+		return
+	}
+	// check for file flag
+	if *file != "" {
+		if _, err := os.Stat(*file); os.IsNotExist(err) {
+			fmt.Println("Error \nFile does not exist:", err)
+			os.Exit(1)
+		}
+		if *encodeFlag {
+			encodeFile(*file)
+
+		} else {
+			decodeFile(*file)
+		}
+		return
 	}
 
-	// let set input to first arguments
-	input := arg[0]
-	result, err := decoder(input)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "decode error:", err)
+	arg := flag.Args()
+	// hangle flags
+	if len(arg) < 1 {
+		displayHelp()
 		os.Exit(1)
 	}
-	fmt.Println(result)
+	// get input from arg 0
+	input := arg[0]
+
+	if *encodeFlag {
+		result, err := encoder(input)
+		if err != nil {
+			fmt.Println("Error")
+			os.Exit(1)
+		}
+		fmt.Println(result) // print the result of encoding
+	} else {
+		result, err := decoder(input)
+		if err != nil {
+			fmt.Println("Error")
+			os.Exit(1)
+		}
+		fmt.Println(result) // print the result of decoding
+
+	}
+
 }
 
 // help flag func
