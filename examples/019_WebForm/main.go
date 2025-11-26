62696ab59b9c4ab7258e3a9aa9adf372de2fb062
Desmond-netw <ddeveh.study@gmail.com>
2025-11-26 06:25:37 +0000

    capitalize data from index and pass to processor

--- examples/019_WebForm/main.go
+++ examples/019_WebForm/main.go
@@ -2,16 +2,60 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
-// index func to read index.html file
-func index(w http.ResponseWriter, req *http.Request) {
-	tmpl := template.Must(template.ParseFiles("template/index.html"))
-	tmpl.Execute(w, "index.html")
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*.html"))
 }
+
 func main() {
-	// web server main entry for handling request and post
-	http.HandleFunc("/", index) // pass index fun as hompage
-	http.ListenAndServe(":3000", nil)
+	// main entry for runing server
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", processor)
+
+	// listen and server on address
+	http.ListenAndServe(":5500", nil)
+
+}
+
+// Render about page thus the corresponding html code to execute
+func index(wr http.ResponseWriter, req *http.Request) {
+	// execute index.html
+	err := tmpl.ExecuteTemplate(wr, "index.html", nil)
+	if err != nil {
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// processor
+func processor(wr http.ResponseWriter, req *http.Request) {
+	// check for valid postmethod and process form data
+	if req.Method != http.MethodPost {
+		http.Redirect(wr, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// grap the data var from input
+	fname := req.FormValue("firster")
+	lname := req.FormValue("laster")
+
+	data := struct {
+		FirstName string
+		LastName  string
+	}{
+		FirstName: fname,
+		LastName:  lname,
+	}
+
+	err := tmpl.ExecuteTemplate(wr, "process.html", data)
+	if err != nil {
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
+		log.Fatal(err)
+		return
+	}
 }
