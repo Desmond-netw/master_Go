8a2004a91febf935a72ec7e778b4ddeaf50ab371
Desmond-netw <dsquare200@gmail.com>
2026-01-04 10:40:20 +0200

    initialize bufio reader func

---
+++ projects/BookStore/Reader.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+// func to read user inputs
+
+func Reader() string {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// reading the input
+	text, _ := reader.ReadString('\n')
+
+	return text
+}
