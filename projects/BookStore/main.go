f580d9a2cc4cb0a941f9fbc9103ec7552026d9a4
Desmond-netw <dsquare200@gmail.com>
2026-01-05 12:20:39 +0200

    Initiliaze car Clients with Car API

--- projects/BookStore/main.go
+++ projects/BookStore/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // Building book inventory
 
 // Author Struct
@@ -20,6 +25,55 @@ type Book struct {
 // func main to grab the entry of the books data
 
 func main() {
-	println("Welcome to the Book Inventory")
+	fmt.Println("Welcome to the Book Inventory")
+	fmt.Println("|-----------------------|")
 
+	// getting books details in variable
+	fmt.Printf("Book Title:")
+	bookTitle := Reader()
+
+	fmt.Printf("Enter  Quanty:")
+	bookQty := Reader()
+
+	fmt.Printf("Enter Unity  Price:")
+	bookPrice := Reader()
+
+	// author details
+	fmt.Printf("Author Name: ")
+	authorName := Reader()
+
+	fmt.Printf("Author Editor: ")
+	authorEditor := Reader()
+
+	fmt.Printf("Author year: ")
+	authorYear := Reader()
+
+	// print brake line
+	fmt.Println()
+
+	// Book data
+	qty, _ := strconv.Atoi(bookQty)
+	book := Book{
+		Title:    bookTitle,
+		Quantity: qty,
+		Price:    bookPrice,
+		BookAuthor: Author{
+			Name:   authorName,
+			Editor: authorEditor,
+			Year:   authorYear,
+		},
+	}
+
+	// Display inventory
+	fmt.Println("\nNew Book Successfully Saved")
+	fmt.Println("|--------------------------|")
+	fmt.Println("Book Details")
+
+	fmt.Println("Title:", book.Title)
+	fmt.Println("Quantity:", book.Quantity)
+	fmt.Println("Price:", book.Price)
+
+	fmt.Println("Author Name:", book.BookAuthor.Name)
+	fmt.Println("Editor:", book.BookAuthor.Editor)
+	fmt.Println("Year:", book.BookAuthor.Year)
 }
