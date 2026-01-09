99aa9d3dd5107f140b48c2f70695b564fca8f065
Desmond-netw <dsquare200@gmail.com>
2026-01-09 08:03:10 +0200

    added  http handler and requests

--- projects/p14_carClient/handlers.go
+++ projects/p14_carClient/handlers.go
@@ -1,34 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"html/template"
 	"net/http"
 )
 
 // handler for templates
-
 var templates = template.Must(
 	template.ParseGlob("templates/*.html"),
 )
-
-func fetchItems(endpoint string) ([]Item, error) {
-	resp, err := http.Get(APIURL + endpoint)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Errorf("HTTP status : %s", resp.Status)
-	}
-
-	// var items to item struct
-	var items []Item
-	err = json.NewDecoder(resp.Body).Decode(&items)
-	return items, err
-}
 
 // home page handler
 func homeHandler(wr http.ResponseWriter, req *http.Request) {
@@ -51,6 +31,9 @@ func listhandler(endpoint string, title string) http.HandlerFunc {
 			Items: items,
 		}
 
-		templates.ExecuteTemplate(w, "layout.html", data)
+		err = templates.ExecuteTemplate(w, "layout.html", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	}
 }
