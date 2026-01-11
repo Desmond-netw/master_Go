c71c85d2e2a092632a87deae1fe8a44b14043349
Desmond-netw <dsquare200@gmail.com>
2026-01-12 00:26:55 +0200

    modified main to print server data to terminal

--- projects/p14_carClient/main.go
+++ projects/p14_carClient/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/models", listhandler("/models", "Car Models"))
-	http.HandleFunc("/manufacturers", listhandler("/manufacturers", "manufacturers"))
-	http.HandleFunc("/categories", listhandler("/categories", "categories"))
+	http.HandleFunc("/models", modelsHandler)
+	http.HandleFunc("/manufacturers", modelsHandler)
+	http.HandleFunc("/categories", modelsHandler)
 
+	fmt.Println("Starting Server.....")
+	fmt.Println("http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
+
 }
