8816669e5334ec4b9d169cd3e300d6c74af0b477
Desmond-netw <dsquare200@gmail.com>
2026-01-11 07:43:19 +0200

    change types to structs.go

---
+++ projects/p14_carClient/structs.go
@@ -0,0 +1,35 @@
+package main
+
+// Manufacture struct
+
+type Manufacturer struct {
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	Country      string `json:"country"`
+	FoundingYear int    `json:"fundingYear"`
+}
+
+// Category struct
+type Category struct {
+	ID   int    `json:"id"`
+	Name string `json:"Name"`
+}
+
+// Car Model Struct represent one model from the API
+type CarModel struct {
+	ID             int            `json:"id"`
+	Name           string         `json:"name"`
+	ManufacturerID int            `json:"manufacturerId"`
+	CategoryId     int            `json:"categoryId"`
+	Year           int            `json:"year"`
+	Specifications Specifications `json:"specification"`
+	Image          string         `json:"image"`
+}
+
+// Specification ( nested struct)
+type Specifications struct {
+	Engine       string `json:"engine"`
+	Horsepower   string `json:"horsepower"`
+	Transmission string `json:"transmission"`
+	Drivetrain   string `json:"drivetrain"`
+}
--- projects/p14_carClient/types.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Item struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Image string `json:"image,omitempty"`
-}
