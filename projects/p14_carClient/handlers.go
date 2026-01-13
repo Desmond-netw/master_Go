b2e05cb85aee3deb064ae38c01c395f73911a697
Desmond-netw <dsquare200@gmail.com>
2026-01-13 10:09:46 +0200

    apply manufacturer handler that send manufacture data to html temp

--- projects/p14_carClient/handlers.go
+++ projects/p14_carClient/handlers.go
@@ -41,6 +41,28 @@ func modelsHandler(wr http.ResponseWriter, req *http.Request) {
 	renderTemplate(wr, "layout.html", carsData)
 }
 
+// Manufacturer handler to send manufacture data to html
+func manufacturerHandler(wr http.ResponseWriter, req *http.Request) {
+	// geting manuf data
+	manufacturer, err := fetchManufacturers()
+	if err != nil {
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	manufacturerData := struct {
+		Title         string
+		Manufacturers []Manufacturer
+	}{
+		Title:         "Manufacturers",
+		Manufacturers: manufacturer,
+	}
+
+	// render template to html output
+	renderTemplate(wr, "layout.html", manufacturerData)
+
+}
+
 // func to handle template rendering
 func renderTemplate(wr http.ResponseWriter, tmpl string, data any) {
 	//passing specific html templates
