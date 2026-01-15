e5f04ec9bc07f8d3805ed1bed84611c4f5acb9fe
Desmond-netw <dsquare200@gmail.com>
2026-01-15 22:32:18 +0200

    add manufacturerMap and CategoryMap s

--- projects/p14_carClient/util.go
+++ projects/p14_carClient/util.go
@@ -1,11 +1,20 @@
-func main
+package main
 
 // Building lookup maps
 
-func manufacturerMap (manufacturer []Manufacturer) map[int]string {
+func manufacturerMap(manufacturer []Manufacturer) map[int]string {
 	manuMap := make(map[int]string)
-for , manuf range manufacturer {
-	manuMap[manuf.ID] = manuf.Name
+	for _, manuf := range manufacturer {
+		manuMap[manuf.ID] = manuf.Name
+	}
+	return manuMap
 }
-return manuMap
+
+// category map
+func categoryMap(category []Category) map[int]string {
+	catMap := make(map[int]string)
+	for _, cats := range category {
+		catMap[cats.ID] = cats.Name
+	}
+	return catMap
 }
